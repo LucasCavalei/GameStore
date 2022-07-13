29c8b62cbae72af3f362c90cf58aa692a1da66a7
LucasCav <lucas.oklp@gmail.com>
2022-07-13 12:18:04 -0300

    Refctoring react-dom

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/signup" element={<Signup />} />
