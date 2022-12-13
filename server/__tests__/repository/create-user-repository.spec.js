9b9a53f673c9a05d79d3e1a403814ec7a114e1d3
LucasCavalei <59570195+LucasCavalei@users.noreply.github.com>
2022-12-13 18:34:59 -0300

    preparing mongoHel0per to mock

--- server/__tests__/repository/create-user-repository.spec.js
+++ server/__tests__/repository/create-user-repository.spec.js
@@ -1,5 +1,7 @@
+import { jest, expect } from '@jest/globals';
 import MongoHelper from '../../helpers/mongo-helper.js';
 import CreateUserRepository from '../../repository/user/create-user-repository.js';
+
 let userModel;
 
 const makeSut = () => {
@@ -29,52 +31,3 @@ describe('Connect  databse by mongoHelper then create a user', () => {
     expect(user.token).toBeTruthy();
   });
 });
-
-//   // afterAll(async () => {
-//   //   await MongoHelper.disconnect();
-//   // });
-
-//   // jest.unstable_mockModule(
-//   //   '../../repository/user/create-user-repository.js',
-//   //   () => {
-//   //     return {
-//   //       __esModule: true,
-//   //       makeSut: jest.fn().mockResolvedValue(() => {
-//   //         'mockedCreateUser';
-//   //       }),
-//   //     };
-//   //   }
-//   // );
-//   // const MongoHelperMock = jest
-//   //   .fn()
-//   //   .mockImplementationOnce(() => 'mockedCreateUser');
-//   // jest.unstable_mockModule('../../helpers/mongo-helper.js', () => {
-//   //   return {
-//   //     __esModule: true,
-//   //     MongoHelper: MongoHelperMock(),
-//   //   };
-//   // });
-
-//   test('should token be valid', async () => {
-//     const MongoHelper = await import('../../helpers/mongo-helper.js');
-//     console.log('MongoRepository 22222222222', MongoHelper);
-//     const makeSut = () => {
-//       return new CreateUserRepository();
-//     };
-//     const sut = makeSut();
-//     const fakeuser = {
-//       name: 'fakeuser',
-//       email: 'fakeuser@example.com',
-//       password: 'fakepassword',
-//     };
-//     await sut.createUser(fakeuser);
-//     // MongoHelper();
-//     expect(MongoHelper).toHaveBeenCalled();
-//     expect(true).toBe(true); // const user = await sut.createUser({
-//     //   name: 'any_name',
-//     //   email: 'any_email@mail.om',
-//     //   password: 'hashed_password',
-//     // });
-//     //  expect(user.token).toBeTruthy();
-//   });
-// });
