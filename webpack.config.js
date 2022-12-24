7b94a40929fdc249f01b2d3befab02a1a0596ffe
LucasCavalei <59570195+LucasCavalei@users.noreply.github.com>
2022-12-24 11:58:01 -0300

    seting webpack config and set wabpack in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,39 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const outputDirectory = 'dist';
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './client/src/index.js',
   output: {
     path: path.join(__dirname, outputDirectory),
     filename: 'bundle.js',
   },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './client/src/index.html',
+      // favicon: "./public/favicon.ico"
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            preset: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
     ],
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:8888',
+    },
   },
 };
