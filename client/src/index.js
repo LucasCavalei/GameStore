e36347f3e03dfd2180aebd2fcb6502b15f804dfb
LucasCavalei <59570195+LucasCavalei@users.noreply.github.com>
2022-12-27 13:34:18 -0300

    Revert "last commit before Desctructure create-react-app"
    This reverts commit 67f73b7270013d66738c4ffa9584597819b3f600.

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './redux/store';
-import { Provider } from 'react-redux';
-
-// https://webpack.js.org/guides/getting-started/#using-a-configuration
-// Here its where javascript initialization  src/index.js in webpack documentation
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
