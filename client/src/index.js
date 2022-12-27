67f73b7270013d66738c4ffa9584597819b3f600
LucasCavalei <59570195+LucasCavalei@users.noreply.github.com>
2022-12-27 01:04:03 -0300

    last commit before Desctructure create-react-app

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import store from "./redux/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+
+// https://webpack.js.org/guides/getting-started/#using-a-configuration
+// Here its where javascript initialization  src/index.js in webpack documentation
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
 
-  document.getElementById("root")
+  document.getElementById('root')
 );
